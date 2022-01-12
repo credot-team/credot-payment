b0a11187ae7307823b3817b50da0a4a4fa14b20b
DuST <dustwk120@gmail.com>
2022-01-12 18:18:10 +0800

    initial commit

---
+++ src/payment/newebpay/api.ts
@@ -0,0 +1,13 @@
+export namespace api {
+  // 單筆交易狀態查詢
+  function queryTradeInfo() {}
+
+  // 取消信用卡授權
+  function creditCardCancel() {}
+
+  // 信用卡請/退款
+  function creditCardClose() {}
+
+  // 電子錢包退款
+  function eWalletRefund() {}
+}
